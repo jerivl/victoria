70c2bd47d5a6ae85267163f53b0017bb351c2859
Jeri V.L. <jerivl@pm.me>
2026-01-11 20:01:58 -0600

    Removed edge dev tools

--- configuration.nix
+++ configuration.nix
@@ -62,11 +62,11 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
-    emacs-nox # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    jujutsu
-    git
-  ];
+  #environment.systemPackages = with pkgs; [
+  #  emacs-nox # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  jujutsu
+  #  git
+  #];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
